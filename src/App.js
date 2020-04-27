3126aa7f7a0ad9e87dbeecece3f6893a14834144
Katerina <katerinalupacheva@gmail.com>
2020-04-28 00:07:49 +0300

    Google auth added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { initClient, renderButton } from "./GoogleAuth/authUtils";
 
 function App() {
+  useEffect(() => {
+    const waitGapiLoad = () => {
+      if (window.gapi.client) {
+        console.log("Client loaded");
+        clearInterval(window.interval);
+        initClient();
+        renderButton();
+      }
+    };
+    window.interval = setInterval(waitGapiLoad, 1000);
+  }, []);
+
   return (
     <div className="App">
       <h2>Google Analytics Dashboard</h2>
+      <div id="signin-button"></div>
     </div>
   );
 }
---
+++ src/GoogleAuth/authUtils.js
@@ -0,0 +1,48 @@
+export const initClient = () => {
+  window.gapi.load("auth2", () => {
+    const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+    window.gapi.auth2
+      .init({
+        client_id: CLIENT_ID,
+        scope: "https://www.googleapis.com/auth/analytics.readonly",
+      })
+      .then(onInit, onError);
+  });
+
+  const onInit = () => {
+    checkSignedIn();
+  };
+
+  const onError = () => {
+    console.error("Error");
+  };
+};
+
+const checkSignedIn = () => {
+  const isSignedIn = window.gapi.auth2.getAuthInstance().isSignedIn.get();
+  if (isSignedIn) {
+    window.gapi.auth2
+      .getAuthInstance()
+      .attachClickHandler("signin-button", {}, onSuccess, onFailure);
+  }
+};
+
+const onSuccess = (googleUser) => {
+  console.log("Logged in as: " + googleUser.getBasicProfile().getName());
+};
+
+const onFailure = (error) => {
+  console.error(error);
+};
+
+export const renderButton = () => {
+  window.gapi.signin2.render("signin-button", {
+    scope: "profile email",
+    width: 240,
+    height: 50,
+    longtitle: true,
+    theme: "dark",
+    onsuccess: onSuccess,
+    onfailure: onFailure,
+  });
+};
